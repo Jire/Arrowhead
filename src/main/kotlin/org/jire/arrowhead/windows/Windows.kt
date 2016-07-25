12c4810b847d92b1eae620578eb180e8e8ed07b7
Jire <technology@live.com>
2016-07-25 17:40:03 -0400

    Updated copyright notice

--- src/main/kotlin/org/jire/arrowhead/windows/Windows.kt
+++ src/main/kotlin/org/jire/arrowhead/windows/Windows.kt
@@ -1,24 +1,24 @@
 /*
-   Copyright 2016 Thomas Nappo
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+ * Copyright 2016 Thomas Nappo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 /*
-   "Windows" is a trademark of Microsoft Corporation.
-
-   The trademark holders are not affiliated with the maker
-   of this product and do not endorse this product.
+ * "Windows" is a trademark of Microsoft Corporation.
+ *
+ * The trademark holders are not affiliated with the maker
+ * of this product and do not endorse this product.
  */
 
 package org.jire.arrowhead.windows
